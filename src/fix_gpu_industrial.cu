4c96a4d1a27aee27d6e5e214e032eac1e5f23b3c
Abdoulaye Balde <111806782+sankBalde@users.noreply.github.com>
2024-10-31 10:25:25 +0100

    version 1 industrial with histogramme OK

--- src/fix_gpu_industrial.cu
+++ src/fix_gpu_industrial.cu
@@ -12,6 +12,15 @@
 #include <thrust/functional.h>
 #include <cub/cub.cuh>
 
+#include <thrust/device_vector.h>
+#include <thrust/transform.h>
+#include <thrust/scatter.h>
+#include <thrust/execution_policy.h>
+#include <thrust/iterator/counting_iterator.h>
+#include <cub/cub.cuh>
+#include <cuda_runtime.h>
+#include <iostream>
+
 struct transform_op
 {
     __host__ __device__
@@ -35,6 +44,9 @@ void fix_image_gpu_industrial(Image& to_fix)
 {
     const int image_size = to_fix.width * to_fix.height;
     const int garbage_val = -27;
+    const int num_bins = 256;  // Nombre de niveaux de gris pour l'histogramme
+    const int min_val = 0;     // Valeur minimale pour l'histogramme
+    const int max_val = 255;   // Valeur maximale pour l'histogramme
 
     // Créer un stream CUDA
     cudaStream_t stream;
@@ -42,21 +54,14 @@ void fix_image_gpu_industrial(Image& to_fix)
 
     // Copier le buffer de `to_fix` vers un `thrust::device_vector`
     thrust::device_vector<int> d_buffer(to_fix.buffer, to_fix.buffer + to_fix.size());
-
-    // Faire une copie profonde de d_buffer pour le scatter final
-    thrust::device_vector<int> d_buffer_copy = d_buffer;
-
-    // Créer le vecteur de prédicat
-    thrust::device_vector<int> predicate(to_fix.size());
+    thrust::device_vector<int> d_buffer_copy = d_buffer; // Copie profonde pour le scatter final
 
     // Générer le vecteur de prédicat
+    thrust::device_vector<int> predicate(to_fix.size());
     thrust::transform(thrust::cuda::par.on(stream), d_buffer.begin(), d_buffer.end(), predicate.begin(),
                       [garbage_val] __device__ (int val) { return val != garbage_val ? 1 : 0; });
-
-    // Effectuer un scan exclusif pour calculer les indices des éléments valides
     thrust::exclusive_scan(thrust::cuda::par.on(stream), predicate.begin(), predicate.end(), predicate.begin(), 0);
 
-    // Appliquer `thrust::scatter_if` pour redistribuer les éléments
     thrust::scatter_if(thrust::cuda::par.on(stream),
                        d_buffer_copy.begin(), d_buffer_copy.end(),  // Données à redistribuer
                        predicate.begin(),                            // Position cible
@@ -64,83 +69,44 @@ void fix_image_gpu_industrial(Image& to_fix)
                        d_buffer.begin(),                             // Sortie
                        [garbage_val] __device__ (int val) { return val != garbage_val; }); // Filtrer les valeurs
 
-    // Transformer les valeurs de d_buffer en fonction de l'index modulo 4
+
+    // Transformer les valeurs de `d_buffer` en fonction de l'index modulo 4
     thrust::transform(thrust::cuda::par.on(stream),
                       thrust::make_zip_iterator(thrust::make_tuple(d_buffer.begin(), thrust::counting_iterator<int>(0))),
                       thrust::make_zip_iterator(thrust::make_tuple(d_buffer.end(), thrust::counting_iterator<int>(image_size))),
                       d_buffer.begin(),
                       transform_op());
 
-    // Synchroniser le stream pour s'assurer que toutes les opérations sont terminées
-    CUDA_CHECK_ERROR(cudaStreamSynchronize(stream));
+    // Allocations pour le calcul de l'histogramme avec CUB
+    thrust::device_vector<int> histogram(num_bins, 0);
+    int *d_histogram = thrust::raw_pointer_cast(histogram.data());
+
+    void* d_temp_storage = nullptr;
+    size_t temp_storage_bytes = 0;
+    cub::DeviceHistogram::HistogramEven(d_temp_storage, temp_storage_bytes, thrust::raw_pointer_cast(d_buffer.data()), d_histogram, num_bins, min_val, max_val + 1, image_size, stream);
+    cudaMalloc(&d_temp_storage, temp_storage_bytes);
+    cub::DeviceHistogram::HistogramEven(d_temp_storage, temp_storage_bytes, thrust::raw_pointer_cast(d_buffer.data()), d_histogram, num_bins, min_val, max_val + 1, image_size, stream);
+
+    // Calcul du CDF pour l'égalisation
+    thrust::device_vector<float> cdf(num_bins);
+    thrust::inclusive_scan(thrust::cuda::par.on(stream), histogram.begin(), histogram.end(), cdf.begin());
+
+    // Normaliser le CDF pour obtenir les valeurs d'égalisation
+    float min_cdf;
+    cudaMemcpy(&min_cdf, thrust::raw_pointer_cast(cdf.data()), sizeof(float), cudaMemcpyDeviceToHost);
+    thrust::transform(thrust::cuda::par.on(stream), cdf.begin(), cdf.end(), cdf.begin(), [min_cdf, image_size] __device__ (float c) {
+        return (c - min_cdf) / (image_size - min_cdf) * 255.0f;
+    });
+
+    // Remapper les valeurs de `d_buffer` pour appliquer l'égalisation
+    thrust::transform(thrust::cuda::par.on(stream), d_buffer.begin(), d_buffer.end(), d_buffer.begin(), [d_cdf = thrust::raw_pointer_cast(cdf.data())] __device__ (int val) {
+        return static_cast<int>(d_cdf[val]);
+    });
 
     // Copier le résultat final dans le buffer d'origine
-    CUDA_CHECK_ERROR(cudaMemcpy(to_fix.buffer, thrust::raw_pointer_cast(d_buffer.data()),
-                                image_size * sizeof(int), cudaMemcpyDeviceToHost));
+    CUDA_CHECK_ERROR(cudaMemcpy(to_fix.buffer, thrust::raw_pointer_cast(d_buffer.data()), image_size * sizeof(int), cudaMemcpyDeviceToHost));
 
-    // Détruire le stream
+    // Libérer les ressources
+    //cudaFree(d_temp_storage);
     CUDA_CHECK_ERROR(cudaStreamDestroy(stream));
 }
-
-
-/*
-bool check_predicate(const thrust::device_vector<int>& predicate_gpu, const std::vector<int>& predicate_cpu) {
-    // Vérifier que les tailles des deux vecteurs sont identiques
-    if (predicate_gpu.size() != predicate_cpu.size()) {
-        std::cerr << "Size mismatch: GPU predicate size (" << predicate_gpu.size()
-                  << ") vs CPU predicate size (" << predicate_cpu.size() << ")" << std::endl;
-        return false;
-    }
-
-    // Créer un vecteur hôte pour recevoir les données du GPU
-    std::vector<int> predicate_gpu_host(predicate_gpu.size());
-
-    // Copier les données du GPU vers le vecteur hôte
-    CUDA_CHECK_ERROR(cudaMemcpy(predicate_gpu_host.data(), thrust::raw_pointer_cast(predicate_gpu.data()),
-                                predicate_gpu.size() * sizeof(int), cudaMemcpyDeviceToHost));
-
-    // Comparer les deux vecteurs élément par élément
-    for (std::size_t i = 0; i < predicate_cpu.size(); ++i) {
-        if (predicate_gpu_host[i] != predicate_cpu[i]) {
-            std::cerr << "Mismatch at index " << i << ": CPU(" << predicate_cpu[i]
-                      << ") vs GPU(" << predicate_gpu_host[i] << ")" << std::endl;
-            return false;
-        }
-    }
-
-    // Si toutes les valeurs sont identiques, les vecteurs sont identiques
-    return true;
-}
-
-// Appeler le kernel de scatter pour réorganiser les éléments
-/*scatter_kernel<<<(to_fix.size() + 255) / 256, 256, 0, d_buffer.stream()>>>(
-    raft::device_span<int>(d_buffer.data(), d_buffer.size()),
-    raft::device_span<int>(predicate.data(), predicate.size()),
-    to_fix.size());
-CUDA_CHECK_ERROR(cudaStreamSynchronize(d_buffer.stream()));
-
-// Réaliser l’égalisation d’histogramme
-thrust::device_vector<int> histogram(256, 0);
-thrust::device_vector<int> d_image(d_buffer.data(), d_buffer.data() + image_size);
-thrust::sort(d_image.begin(), d_image.end());
-thrust::counting_iterator<int> search_begin(0);
-thrust::upper_bound(d_image.begin(), d_image.end(), search_begin, search_begin + 256, histogram.begin());
-
-// Calculer le CDF
-thrust::exclusive_scan(histogram.begin(), histogram.end(), histogram.begin());
-
-// Trouver cdf_min
-//! error avec copy_if
-/*int cdf_min;
-thrust::copy_if(histogram.begin(), histogram.end(), thrust::make_counting_iterator(1), &cdf_min,
-                [] __device__(int x) { return x != 0; });
-
-// Appliquer l’égalisation
-equalize_kernel<<<(image_size + 255) / 256, 256, 0, d_buffer.stream()>>>(
-    raft::device_span<int>(d_buffer.data(), d_buffer.size()),
-    raft::device_span<int>(d_buffer.data(), d_buffer.size()),
-    raft::device_span<int>(histogram.data().get(), 256),
-    cdf_min, image_size);*/
-
-//CUDA_CHECK_ERROR(cudaMemcpy(to_fix.buffer, d_buffer.data(), image_size * sizeof(int), cudaMemcpyDeviceToHost));
-
